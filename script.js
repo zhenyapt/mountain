2097b7aaa1b0b6ac3922ba1bd795bb8d362ff81e
eva <pitko.evgeniya@gamil.com>
2024-05-20 14:39:08 +0300

    add close btn

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 'use strict';
 
-// let xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://goweather.herokuapp.com/weather/Moscow');
-// xhr.send();
-
-// xhr.addEventListener('load', function () {
-//   console.log(`Загружено: ${xhr.status} ${xhr.response}`);
-// });
-
-// console.log(23);
-
 let btnsOpen = document.querySelectorAll('.btn-open-modal');
+let btnClose = document.querySelector('.btn-close-modal');
 let modal = document.querySelector('.popup');
-let btnClose = document.querySelector('.btn-close');
 let modalContent = document.querySelector('.popup-content');
-let modalContentHTML;
-let btn;
 
 let mobileMenuIcon = document.querySelector('.mobile-menu-icon');
 let mainMenu = document.querySelector('.nav-list');
 let mobileMenu = document.querySelector('.mobile-menu');
 let mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
+let modalContentHTML;
 
 mobileMenuIcon.addEventListener('click', function (event) {
   console.log(mainMenu);
@@ -32,7 +20,7 @@ mobileMenuIcon.addEventListener('click', function (event) {
 
 for (let btnOpen of btnsOpen) {
   btnOpen.addEventListener('click', function (event) {
-    btn = event.target.closest('.btn-open-modal');
+    let btn = event.target.closest('.btn-open-modal');
     console.log(btn);
     createModalcontent(btn);
     modal.classList.toggle('hide');
@@ -41,14 +29,14 @@ for (let btnOpen of btnsOpen) {
 }
 
 function createModalcontent(btn) {
-  if (btn.getAttribute('data-type-content') == 'form') {
+  let btnType = btn.getAttribute('data-type-content');
+  if (btnType == 'form') {
     console.log('form');
-    modalContentHTML =
-      '<form class="form-consultation form"> <h2 class="form__heading">Заказать консультацию</h2> <input type="text" class="form-name form-input" placeholder="Ваше имя"/> <input type="tel" class="form-tel form-input" placeholder="Ваш телефон" /> <button class="btn btn-dark btn-form-consultation" type="button"> Отправить </button> </form>';
+    modalContentHTML = `<h2 class="form__heading">Заказать консультацию</h2> <form class="form-consultation form"> <input type="text" class="form-name form-input" placeholder="Ваше имя"/> <input type="tel" class="form-tel form-input" placeholder="Ваш телефон" /> <button class="btn btn-dark btn-form-consultation" type="button"> Отправить </button> </form>`;
     modalContent.innerHTML = modalContentHTML;
   }
 
-  if (btn.getAttribute('data-type-content') == 'video') {
+  if (btnType == 'video') {
     let codeVideo = btn.getAttribute('data-video-code');
     modalContentHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${codeVideo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
     modalContent.innerHTML = modalContentHTML;
@@ -57,10 +45,11 @@ function createModalcontent(btn) {
 
 function attachModalEvents() {
   modal.addEventListener('click', handleOutside);
+  btnClose.addEventListener('click', closeModal);
 }
 
 function handleOutside(event) {
-  const isClickOutside = !!event.target.closest('.popup-content');
+  const isClickOutside = !!event.target.closest('.popup-wrapper');
   if (!isClickOutside) {
     closeModal();
   }
@@ -69,6 +58,7 @@ function handleOutside(event) {
 function closeModal() {
   modal.classList.toggle('hide');
   modal.removeEventListener('click', handleOutside);
+  modal.removeEventListener('click', closeModal);
 }
 
 const swiper = new Swiper('.swiper', {
