bad0fffbf80b217dd1b4fe4bea7380f1abe441aa
eva <pitko.evgeniya@gamil.com>
2024-05-28 23:52:02 +0300

    add tel mask, add subscribe form, add success message

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let btnsOpen = document.querySelectorAll('.btn-open-modal');
 let btnClose = document.querySelector('.btn-close-modal');
+
 let modal = document.querySelector('.popup');
 let modalContent = document.querySelector('.popup-content');
 let modalContentHTML;
@@ -22,23 +23,26 @@ mobileMenuIcon.addEventListener('click', function (event) {
 for (let btnOpen of btnsOpen) {
   btnOpen.addEventListener('click', function (event) {
     let btn = event.target.closest('.btn-open-modal');
-    console.log(btn);
     createModalcontent(btn);
-    modal.classList.toggle('hide');
+    modal.classList.remove('hide');
     attachModalEvents();
   });
 }
 
-function createModalcontent(btn) {
-  let btnType = btn.getAttribute('data-type-content');
-  if (btnType == 'form') {
-    console.log('form');
-    modalContentHTML = `<h2 class="form__heading">Заказать консультацию</h2> <form class="form-consultation form"> <input type="text" class="form-name form-input" placeholder="Ваше имя"/> <input type="tel" class="form-tel form-input" placeholder="Ваш телефон" /> <button class="btn btn--dark btn-form-consultation" type="button"> Отправить </button> </form>`;
-    modalContent.innerHTML = modalContentHTML;
-  }
+function createModalcontent(button) {
+  let btnType = button.getAttribute('data-type-content');
 
-  if (btnType == 'video') {
-    let codeVideo = btn.getAttribute('data-video-code');
+  if (btnType == 'form') {
+    modalContentHTML = `<h2 class="form__heading">Заказать консультацию</h2> <form class="form-consultation form"> 
+    <input type="text" class="form-name form-input" placeholder="Ваше имя"/> 
+    <input type="tel" class="form-tel form-input" id="form-input-tel" placeholder="Ваш телефон" /> 
+    <button class="btn btn--dark btn-form" type="button"> Отправить </button> </form>`;
+
+    modalContent.innerHTML = modalContentHTML;
+
+    handlerPopupForm();
+  } else if (btnType == 'video') {
+    let codeVideo = button.getAttribute('data-video-code');
     modalContentHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${codeVideo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
     modalContent.innerHTML = modalContentHTML;
   }
@@ -56,15 +60,47 @@ function handleOutside(event) {
   }
 }
 
-function closeModal() {
-  modal.classList.toggle('hide');
+function closeModal(event) {
+  event.stopPropagation();
+  modal.classList.add('hide');
   modal.removeEventListener('click', handleOutside);
   btnClose.removeEventListener('click', closeModal);
 }
 
 function closeMobileMenu() {
-  mobileMenuWrapper.classList.toggle('hide');
+  mobileMenuWrapper.classList.add('hide');
   btnCloseMenu.removeEventListener('click', btnCloseMenu.closeMobileMenu);
+}
+
+function handlerPopupForm() {
+  //получение созданной попап формы
+  let currentPopupForm = document.querySelector('.popup .form');
+  let telephoneCurentPopupForm = currentPopupForm.querySelector('.form-tel');
+  //добавление маски телефону
+  if (telephoneCurentPopupForm) {
+    setMaskTel(telephoneCurentPopupForm);
+  }
+  // добавление обработчика кнопке отправки формы -- нужно написать
+  let btnCurrentPopupForm = currentPopupForm.querySelector('.btn-form');
+
+  btnCurrentPopupForm.addEventListener('click', showSuccessMessage);
+
+  //проверка формы -- нужно написать
+  //отправка формы -- нужно написать
+  //показ сообщения - об успешности отправки (нужен результат проверок)
+}
+
+function setMaskTel(telephoneCurentPopupForm) {
+  let maskOptions = {
+    mask: '+{7}(000)000-00-00',
+  };
+  let mask = IMask(telephoneCurentPopupForm, maskOptions);
+}
+
+function showSuccessMessage(event) {
+  event.stopPropagation();
+  modalContentHTML = `<h2 class="form__heading">Форма успешно отправлена</h2> <p>Мы перезвоним Вам в ближайщее время</p>`;
+  modalContent.innerHTML = modalContentHTML;
 }
 
 const swiperDirection = new Swiper('.popular-direction-slider', {
@@ -123,9 +159,16 @@ let fullImage = document.createElement('img');
 let popupPhoto = document.querySelector('.popup-photo');
 for (let photo of photos) {
   photo.addEventListener('click', function (event) {
-    console.log(event.target.src);
+    let srcImgFull;
+    if (event.target.classList.contains('photo-gallery__item')) {
+      srcImgFull = event.target.firstElementChild.src;
+    } else if (event.target.classList.contains('photo-gallery__img')) {
+      srcImgFull = event.target.src;
+    } else {
+      console.log('error');
+    }
 
-    fullImage.setAttribute('src', event.target.src);
+    fullImage.setAttribute('src', srcImgFull);
     fullImage.classList.add('photo-gallery__img-full');
     popupPhoto.classList.toggle('hide');
     popupPhoto.prepend(fullImage);
